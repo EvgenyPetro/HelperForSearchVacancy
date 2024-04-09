f683da9fd08daacdaec77cfbe69c398fa8f7fce4
Evgeny Petrov <petroves88@mail.ru>
2024-04-09 19:21:45 +0300

    add Head Hunter payload DTO

--- vacancy-import-service/src/main/java/ru/petrov/vacancyimportservice/client/HHVacancyRestClient.java
+++ vacancy-import-service/src/main/java/ru/petrov/vacancyimportservice/client/HHVacancyRestClient.java
@@ -1,8 +1,10 @@
 package ru.petrov.vacancyimportservice.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+import ru.petrov.vacancyimportservice.dto.HHPayloadDto;
 import ru.petrov.vacancyimportservice.dto.Vacancy;
 
 import java.util.Collections;
@@ -11,17 +13,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HHVacancyRestClient implements VacancyRestClient {
 
+    private static final ParameterizedTypeReference<List<HHPayloadDto>> VACANCY_TYPE_REF =
+            new ParameterizedTypeReference<>() {
+            };
+
     private final RestClient restClient;
 
     @Override
     public List<Vacancy> getVacancy() {
 
-        String result = restClient
+        var result = restClient
                 .get()
                 .uri("/vacancies?text=java")
                 .retrieve()
-                .body(String.class);
-        System.out.println(result);
+
+                .body(HHPayloadDto.class);
+        System.out.println(result.getItems().get(1));
         return Collections.emptyList();
     }
 }
